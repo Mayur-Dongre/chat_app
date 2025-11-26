77867a611ff5e47296d1dce47d516f5d3134b6cb
Mayur-Dongre <maddymrd07@gmail.com>
2025-11-27 04:19:34 +0530

    JST, Users in the auth Backend

--- auth-be/index.js
+++ auth-be/index.js
@@ -1,24 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./auth/auth.route.js";
+import usersRouter from "./auth/users.route.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+	cors({
+		credentials: true,
+		origin: ["http://localhost:3000", "http://localhost:3001"]
+	})
+);
 
 app.use("/auth", authRouter);
+app.use("/users", usersRouter);
 
 // Define a route
 app.get("/", (req, res) => {
-  res.send("Congratulations Mad World Folks!");
+	res.send("Congratulations Mad World Folks!");
 });
 
 // Start the server
 app.listen(PORT, (req, res) => {
-  connectToMongoDB();
-  console.log(`Server is listening at http://localhost:${PORT}`);
+	connectToMongoDB();
+	console.log(`Server is listening at http://localhost:${PORT}`);
 });
