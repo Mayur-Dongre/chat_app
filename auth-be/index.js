0447ef1174ebeb6ca9640b84de62676fe50649b4
Mayur-Dongre <maddymrd07@gmail.com>
2025-11-29 12:50:32 +0530

    Allowed CORS origins added

--- auth-be/index.js
+++ auth-be/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(
 	cors({
 		credentials: true,
-		origin: ["http://localhost:3000", "http://localhost:3001"]
+		origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
 	})
 );
 
